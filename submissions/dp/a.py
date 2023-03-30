df1946112fadeb6df474c4713afba871949cc9c6
Tom0rrow <tomohiro.sakamoto@icloud.com>
2023-03-30 22:06:15 +0900

    add submission: 2023/03/30 22:06:14

--- submissions/dp/a.py
+++ submissions/dp/a.py
@@ -1,14 +1,12 @@
 N = int(input())
-h = list(map(int, input().split()))
+h = list(map(int,input().split()))
 
 dp = [0]*N
-
 dp[0] = 0
 dp[1] = abs(h[1]-h[0])
-
-for i in range(2, N):
-    one_jump = dp[i-2] + abs(h[i]-h[i-2])
-    two_jump = dp[i-1] + abs(h[i]-h[i-1])
-    dp[i] = min(one_jump, two_jump)
-
+for i in range(2,N):
+    d1 = dp[i-1]+abs(h[i]-h[i-1])
+    d2 = dp[i-2]+abs(h[i]-h[i-2])
+    dp[i] = min(d1,d2)
+    
 print(dp[-1])
